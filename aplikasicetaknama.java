00926d37c7f60b324d3fb4e01c459178337f339f
Natasha Ursipuny <90620581+natashaursipuny@users.noreply.github.com>
2021-09-22 19:27:18 -0700

    Add files via upload

---
+++ aplikasicetaknama.java
@@ -0,0 +1,15 @@
+public class aplikasicetaknama {
+	public static void main (String[] args){
+
+
+		// ini adalah fungsi pemcetakan 
+		
+			System.out.println("tasya sangat" + " cantik");
+			
+	
+	}
+
+
+}
+
+	
